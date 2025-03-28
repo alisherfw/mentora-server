5d42b494966d9b7a9430e87cc151e3198197a9d5
Alisherbek Abduolimov <alisherfw@gmail.com>
2025-03-28 14:17:06 +0900

    added course update put method

--- routes/Course.js
+++ routes/Course.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const Course = require('../models/Course')
-const { authenticate } = require('../middleware/auth')
+const { authenticate, checkOwnership } = require('../middleware/auth')
 const Chapter = require('../models/Chapter')
 const Unit = require('../models/Unit')
 const router = express.Router()
@@ -24,7 +24,7 @@ router.get("/", async (req, res) => {
         })
 
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
@@ -32,35 +32,35 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
     const id = req.params.id;
 
-    if(!mongoose.isValidObjectId(id)) {
-        return res.status(400).json({message: "Invalid course ID format"})
+    if (!mongoose.isValidObjectId(id)) {
+        return res.status(400).json({ message: "Invalid course ID format" })
     }
 
     try {
         const course = await Course.findById(id)
             .populate("chapters")
             .populate({ path: "chapters", populate: { path: "units" } });
-            
-        if(!course) {
-            return res.status(404).json({message: "Course not found"})
+
+        if (!course) {
+            return res.status(404).json({ message: "Course not found" })
         }
 
         res.status(200).json(course);
 
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
 // Create course
-router.post("/create", authenticate, async(req, res) => {
-    
+router.post("/create", authenticate, async (req, res) => {
+
     try {
 
-        const {title, description, accessType, chapters} = req.body;
+        const { title, description, accessType, chapters } = req.body;
 
-        if(!title) {
-            return res.status(400).json({message: "Title is required"});
+        if (!title) {
+            return res.status(400).json({ message: "Title is required" });
         }
 
         const newCourse = new Course({
@@ -72,22 +72,21 @@ router.post("/create", authenticate, async(req, res) => {
 
         await newCourse.save();
 
-        if(chapters && chapters.length > 0) {
+        if (chapters && chapters.length > 0) {
             for (const chapter of chapters) {
                 const newChapter = new Chapter({
                     title: chapter.title,
                     courseId: newCourse._id
                 });
-                
+
                 await newChapter.save();
 
-                if(chapter.units && chapter.units.length > 0) {
+                if (chapter.units && chapter.units.length > 0) {
                     for (const unit of chapter.units) {
                         const newUnit = new Unit({
                             title: unit.title,
                             contents: unit.contents,
-                            chapterId: newChapter._id,
-                            order: unit.order
+                            chapterId: newChapter._id
                         })
 
                         await newUnit.save();
@@ -98,12 +97,93 @@ router.post("/create", authenticate, async(req, res) => {
 
         }
 
-        res.status(201).json({message: "Course created successfully", course: newCourse})
+        res.status(201).json({ message: "Course created successfully", course: newCourse })
 
     } catch (error) {
-        res.status(500).json({error: error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
+// Update course 
+
+router.put("/:id", authenticate, checkOwnership, async (req, res) => {
+    try {
+        const { title, description, accessType, chapters } = req.body;
+        const courseId = req.params;
+
+        if (!title) {
+            return res.status(400).json({ message: "Title is required" })
+        }
+
+        const course = await Course.findById(courseId);
+        if (!course) {
+            return res.status(404).json({ message: "Course not found!" })
+        }
+
+        course.title = title;
+        course.description = description || "";
+        course.accessType = accessType || "public";
+        await course.save();
+
+        const existingChapters = await Chapter.find({ courseId }).lean();
+
+        const updatedChapters = new Set();
+
+        for (const chapter of chapters) {
+            let chapterDoc = existingChapters.find(ch => ch._id.toString() === chapter._id);
+
+            if (chapterDoc) {
+                await Chapter.findByIdAndUpdate(chapterDoc._id, { title: chapter.title })
+            } else {
+                chapterDoc = new Chapter({ title: chapter.title, courseId });
+                await chapterDoc.save();
+            }
+
+            updatedChapters.add(chapterDoc._id.toString())
+
+            const existingUnits = await Unit.find({ chapterId: chapterDoc._id }).lean();
+            const updatedUnitIds = new Set();
+
+            for (const unit of chapter.units) {
+                let unitDoc = existingUnits.find(u => u._id.toString === unit._id);
+
+                if (unitDoc) {
+                    await Unit.findByIdAndUpdate(unitDoc._id, {
+                        title: unit.title,
+                        contents: unit.contents
+                    })
+                } else {
+                    unitDoc = new Unit({
+                        chapterId: chapterDoc._id,
+                        title: unit.title,
+                        contents: unit.contents
+                    })
+                    await unitDoc.save();
+                }
+                updatedUnitIds.add(unitDoc._id.toString());
+
+            }
+
+            await Unit.deleteMany({
+                chapterId: chapterDoc._id,
+                _id: { $nin: Array.from(updatedUnitIds) }
+            });
+
+        }
+
+        await Chapter.deleteMany({
+            courseId,
+            _id: { $nin: Array.from(updatedChapters) }
+        })
+
+        res.status(200).json({
+            message: "Course updated successfully",
+            course
+        })
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
 
 module.exports = router;
