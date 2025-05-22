ab9486fbdb4912f2825c3cb0733d755065b49ae1
Alisherbek Abduolimov <alisherfw@gmail.com>
2025-05-23 02:43:28 +0900

    port updated

--- Dockerfile
+++ Dockerfile
@@ -14,7 +14,7 @@ RUN npm install
 COPY . .
 
 # Expose the port your app runs on
-EXPOSE 3000
+EXPOSE 5000
 
 # Start the application
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -33,5 +33,5 @@ app.use("/api/courses", courseRoute)
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`) }))
+    .then(() => app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}, mongoDB connected`) }))
     .catch(err => console.log(err))
